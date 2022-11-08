766bc9a74c7291ca458a2d0607a9bb664e89c344
SerikovZhalgas <zhalgasserikov@gmail.com>
2022-11-09 00:08:32 +0600

    Updates

---
+++ static/js/809.2d339d2c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[809],{4809:function(a,u,s){s.r(u);s(2791);var e=s(184);u.default=function(){return(0,e.jsx)("div",{})}}}]);
+//# sourceMappingURL=809.2d339d2c.chunk.js.map
